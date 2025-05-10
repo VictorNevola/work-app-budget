0fe6516e82b665bfcc4da4af8a23d7b2712f6140
Victor Nevola <victor_nevola@carrefour.com>
2025-05-09 21:15:44 -0300

    feat: add mongodb connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,11 @@
-package cmd
+package main
 
-func main() {}
+import "github.com/VictorNevola/work-app-budget/configuration/database"
+
+func main() {
+	_, disconnectDB := database.NewMongoDB()
+
+	defer func() {
+		disconnectDB()
+	}()
+}
---
+++ configuration/database/mongodb.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type (
+	Database struct {
+		*mongo.Database
+	}
+)
+
+func NewMongoDB() (*Database, func()) {
+	uri := "mongodb://root:example@localhost:27017/"
+	mainDatabase := "work-app-budget"
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic("Failed to connect to Mongo \n Error: " + err.Error())
+	}
+
+	if err := client.Ping(context.TODO(), nil); err != nil {
+		panic("Failed to connect to Mongo \n Error: " + err.Error())
+	}
+
+	disconnectFunction := func() {
+		client.Disconnect(context.TODO())
+	}
+
+	return &Database{
+		Database: client.Database(mainDatabase),
+	}, disconnectFunction
+}
